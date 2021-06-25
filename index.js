0d0b6e743bdde9663966c3849a4601a62abf6d8f
amongiove <amandamongiove@gmail.com>
2021-06-25 16:45:40 -0400

    commit tree bd824adb6acb66c418805a578e1b3442aa03711f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(array, callback){
+    const r = [];
+    for (let i = 0; i < array.length; i++) {
+        const theElement = array[i];
+        r.push(callback(theElement));
+    }
+    return r;
+}
+
+function reduce(array, callback, start) {
+    let total;
+    if (start) {
+        total = start;
+        for (let i = 0; i < array.length; i++) {
+            total = callback(array[i], total);
+    }
+    return total;
+    } else {
+        total = array[0];
+        for (let i = 1; i < array.length; i++) {
+            total = callback(array[i], total);
+        }
+        return total;
+    }
+}
